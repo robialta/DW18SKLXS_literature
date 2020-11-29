b35b0894c7a388e3ef64ac0614ba25e5042e24f5
Robialta <obyaltha@gmail.com>
2020-11-29 19:21:22 +0800

    Update base url to production backend

--- src/config/api.js
+++ src/config/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const API = axios.create({
-    // baseURL: "https://api-literature.herokuapp.com/api/v1",
-    baseURL: "http://localhost:5000/api/v1",
+    baseURL: "https://api-literature.herokuapp.com/api/v1", //Production
+    // baseURL: "http://localhost:5000/api/v1", //Local
 });
 
 export const setAuthToken = (token) => {
